548e941143a364793ffe897a17a5d5cd623178ef
Jessi <jessisibayan@gmail.com>
2021-08-01 02:26:27 +0800

    Changed Bootstrap to MDB

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // require('@/assets/css/bootstrap-darkly.css')
-require('@/assets/css/bootstrap-flatly.css')
-import 'bootstrap/dist/js/bootstrap.min.js'
+// require('@/assets/css/bootstrap-flatly.css')
+// import 'bootstrap/dist/js/bootstrap.min.js'
 import router from './router'
 // import 'jquery/src/jquery.js'
 
