eb6b2d5827e7df94467aa09c14c1ccaabe53596a
Jessi <jessisibayan@gmail.com>
2021-08-05 18:16:46 +0800

    Migrated gun.js to ES6
    - Optimized code
    - Changed logic in calculating data(stk and ttk)

--- src/assets/js/gun.js
+++ src/assets/js/gun.js
@@ -1,55 +1,59 @@
 
-function Gun(gun)
+class Gun
 {
-    this.id = gun.id
-    this.name = gun.name
-    this.type = gun.type
-    this.damage = gun.damage
-    this.range = gun.range
-    this.firerate = gun.firerate
-    this.hitbox = gun.hitbox
-
-    this.generateDamageProfile = function()
+    constructor(gun)
     {
-        if (this.range.length < this.damage.length) this.range.push(100)
-    
-        let ctr = 0
-        let damageProfile = []
+        this.id = gun.id
+        this.name = gun.name
+        this.type = gun.type
+        this.damage = gun.damage
+        this.range = gun.range
+        this.firerate = gun.firerate
+        this.hitbox = gun.hitbox
+        if ('burst' in gun)
+            this.burst = gun.burst
+    }
+
+    sttk(params)
+    {
+        let reduction = (100 - params.vest) / 100
+        let multiplier = (params.hitbox in this.hitbox) ? this.hitbox[params.hitbox] : 1
+
+        let sttk = {
+            stk: [],
+            ttk: []
+        }
+
+        if (this.range.length < this.damage.length)
+            this.range.push(params.maxRange ?? 100)
+
+        let c = 0
         for (let x = 0; x < this.damage.length; x++)
         {
-            for (let y = ctr; y <= this.range[x]; y++)
+            let damage = Math.trunc(this.damage[x] * multiplier * reduction)
+            let stk = Math.ceil(params.hp / damage)
+            let burstCount, burstDelay
+
+            if ('burst' in this)
             {
-                damageProfile.push(this.damage[x])
-                ctr++
+                burstCount = Math.ceil(stk / this.burst.rounds)
+                burstDelay = this.burst.delay * (burstCount - 1)
+            }
+
+            for (let y = c; y <= this.range[x]; y++)
+            {
+                sttk.stk.push(stk)
+
+                if ('burst' in this)
+                    sttk.ttk.push(Math.trunc(60000 / this.firerate * (stk - burstCount)) + burstDelay)
+                else
+                    sttk.ttk.push(Math.trunc(60000 / this.firerate * (stk - 1)))
+                
+                c++
             }
         }
 
-        return damageProfile
-    }
-
-    this.computeStk = function(params)
-    {
-        let vestReduction = (100 - params.vest) / 100
-        let hitboxMultiplier
-
-        if (params.hitbox in this.hitbox) hitboxMultiplier = this.hitbox[params.hitbox]
-        else hitboxMultiplier = 1
-        
-        let damage = Math.trunc(
-            params.damageProfile[params.range] * hitboxMultiplier * vestReduction
-        )
-        let health = params.hp
-
-        return Math.ceil(
-            health / damage
-        )
-    }
-
-    this.computeTtk = function(params)
-    {
-        return Math.trunc(
-            60000 / this.firerate * (params.stk - 1)
-        )
+        return sttk
     }
 }
 
