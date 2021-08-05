c40ba7b5ae885c87f35accb66d45232633f93d72
Jessi <jessisibayan@gmail.com>
2021-08-05 19:43:12 +0800

    Removed Navbar component import (temporarily)

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,12 @@
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
-export default {
-  components: {
-    Navbar
-  }
-}
+// import Navbar from '@/components/Navbar.vue'
+// export default {
+//   components: {
+//     Navbar
+//   }
+// }
 </script>
 
 <style>
