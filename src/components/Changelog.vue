8872c7a65dcbea0e159e2e5a9c07b3035564d896
Jessi <jessisibayan@gmail.com>
2021-09-07 00:26:15 +0800

    Updated changelog (Sep 7, 2021)

--- src/components/Changelog.vue
+++ src/components/Changelog.vue
@@ -2,9 +2,27 @@
     <div class="container">
         <div class="row justify-content-center my-3">
             <div class="col-12 col-md-6">
+
+                <h3>Season 7 (2021)</h3>
+
+                <div>
+                    <sup>September 7, 2021</sup>
+                    <ul>
+                        <li>Added AK-47 (5.45mm) stats</li>
+                        <li>Added Man-O-War stats</li>
+                        <li>Added DR-H and DR-H (OTM) stats</li>
+                        <li>Added CR-56 (M67) stats</li>
+                        <li>Added S36 stats</li>
+                        <li>Added UL736 stats</li>
+                        <li>Added HG 40 stats</li>
+                        <li>Added GKS stats</li>
+                        <li>Added Cordite stats</li>
+                        <li>Added QQ9 (10mm) stats</li>
+                        <li>Fixed CR-56 AMAX damage and hitbox stats</li>
+                    </ul>
+                </div>
                 
                 <div>
-                    <h3>Season 7 (2021)</h3>
                     <sup>August 26, 2021</sup>
                     <ul>
                         <li>Added Hades stats</li>
