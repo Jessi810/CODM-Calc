05bd7c1b2b3fc72c1d778dcc1b1b432f00dbf0a7
Jessi <jessisibayan@gmail.com>
2021-09-07 19:39:35 +0800

    Added footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-10 col-md-8">
+
+                <hr>
+
+                Find anything wrong? Send a message on:&nbsp;
+
+                <a href="https://discord.gg/88EdHfT8FT">
+                    <i class="bi bi-discord"></i>
+                    Discord
+                </a>
+
+                &nbsp;
+
+                <a href="https://www.reddit.com/user/CrazyAmount5">
+                    <i class="bi bi-reddit"></i>
+                    Reddit
+                </a>
+
+                <br>
+
+                Contributions are welcome
+
+                <a href="https://github.com/Jessi810/CODM-Calc">
+                    <i class="bi bi-github"></i>
+                    Github
+                </a>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
