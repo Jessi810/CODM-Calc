b253c955ff47f4663af8c64a38a3398fcef82db7
Jessi810 <jessisibayan@gmail.com>
2021-10-07 18:54:49 +0800

    Fixed CR-56 AMAX (M67) damage stats
    Removed unused data

--- src/gun-stats.js
+++ src/gun-stats.js
@@ -2,7 +2,6 @@ const gunStats = [
     {
         id: 1010,
         name: 'Type 25',
-        type: 'AR',
         damage: [24, 22, 19, 16],
         range: [10, 17, 25],
         firerate: 769,
@@ -14,7 +13,6 @@ const gunStats = [
     {
         id: 1020,
         name: 'M16',
-        type: 'AR', 
         damage: [30, 27, 22],
         range: [21, 41],
         firerate: 788,
@@ -31,7 +29,6 @@ const gunStats = [
     {
         id: 1030,
         name: 'AK117',
-        type: 'AR',
         damage: [24, 20, 16],
         range: [20, 36],
         firerate: 769,
@@ -43,7 +40,6 @@ const gunStats = [
     {
         id: 1040,
         name: 'AK-47',
-        type: 'AR',
         damage: [33, 26, 23, 21],
         range: [20, 30, 55],
         firerate: 550,
@@ -54,7 +50,6 @@ const gunStats = [
     {
         id: 1041,
         name: 'AK-47 (5.45)',
-        type: 'AR',
         damage: [25, 21, 16, 15],
         range: [14, 24, 35],
         firerate: 750,
@@ -65,7 +60,6 @@ const gunStats = [
     {
         id: 1050,
         name: 'ASM10',
-        type: 'AR',
         damage: [34, 26, 20],
         range: [15, 33],
         firerate: 550,
@@ -76,7 +70,6 @@ const gunStats = [
     {
         id: 1060,
         name: 'M4',
-        type: 'AR',
         damage: [24, 20, 19],
         range: [16, 36],
         firerate: 682,
@@ -88,7 +81,6 @@ const gunStats = [
     {
         id: 1070,
         name: 'BK57',
-        type: 'AR', 
         damage: [27, 22, 19],
         range: [18, 30],
         firerate: 665,
@@ -99,7 +91,6 @@ const gunStats = [
     {
         id: 1080,
         name: 'LK24',
-        type: 'AR',
         damage: [26, 22, 18],
         range: [23, 38],
         firerate: 633,
@@ -115,7 +106,6 @@ const gunStats = [
     {
         id: 1090,
         name: 'ICR-1',
-        type: 'AR',
         damage: [26, 24, 20, 19],
         range: [16, 26, 36],
         firerate: 630,
@@ -127,7 +117,6 @@ const gunStats = [
     {
         id: 1100,
         name: 'Man-O-War',
-        type: 'AR',
         damage: [37, 30, 24],
         range: [23, 43],
         firerate: 500,
@@ -138,7 +127,6 @@ const gunStats = [
     {
         id: 1110,
         name: 'KN-44',
-        type: 'AR',
         firerate: 633,
         damage: [26, 24, 19],
         range: [20, 33],
@@ -150,7 +138,6 @@ const gunStats = [
     {
         id: 1120,
         name: 'HBRa3',
-        type: 'AR', 
         damage: [26, 22, 17],
         range: [14, 36],
         firerate: 697,
@@ -161,7 +148,6 @@ const gunStats = [
     {
         id: 1130,
         name: 'HVK-30',
-        type: 'AR', 
         damage: [23, 18, 16],
         range: [22, 34],
         firerate: 711,
@@ -172,7 +158,6 @@ const gunStats = [
     {
         id: 1131,
         name: 'HVK-30 (LC)',
-        type: 'AR', 
         damage: [27, 24, 17],
         range: [5, 26],
         firerate: 711,
@@ -188,7 +173,6 @@ const gunStats = [
     {
         id: 1140,
         name: 'DR-H',
-        type: 'AR',
         damage: [32, 31, 25, 24, 19],
         range: [14, 17, 25, 35],
         firerate: 570,
@@ -199,7 +183,6 @@ const gunStats = [
     {
         id: 1141,
         name: 'DR-H (OTM)',
-        type: 'AR',
         damage: [32, 31, 25, 24, 19],
         range: [14, 17, 25, 35],
         firerate: 570,
@@ -211,7 +194,6 @@ const gunStats = [
     {
         id: 1150,
         name: 'Peacekeeper MK2',
-        type: 'AR',
         damage: [26, 24, 19, 18],
         range: [11, 26, 41],
         firerate: 652,
@@ -223,7 +205,6 @@ const gunStats = [
     {
         id: 1151,
         name: 'Peacekeeper MK2 (Rapid Fire)',
-        type: 'AR',
         damage: [31, 29, 18, 16],
         range: [6, 16, 26],
         firerate: 740,
@@ -235,7 +216,6 @@ const gunStats = [
     {
         id: 1152,
         name: 'Peacekeeper MK2 (Well-Forged)',
-        type: 'AR',
         damage: [26, 24, 19, 18],
         range: [16, 39, 61],
         firerate: 652,
@@ -247,7 +227,6 @@ const gunStats = [
     {
         id: 1153,
         name: 'Peacekeeper MK2 (Taskforce)',
-        type: 'AR',
         damage: [26, 24, 19, 18],
         range: [16, 39, 61],
         firerate: 711,
@@ -259,7 +238,6 @@ const gunStats = [
     {
         id: 1154,
         name: 'Peacekeeper MK2 (Long-Range)',
-        type: 'AR',
         damage: [26, 24, 19, 18],
         range: [22, 52, 82],
         firerate: 652,
@@ -271,7 +249,6 @@ const gunStats = [
     {
         id: 1155,
         name: 'Peacekeeper MK2 (Integral Suppressor)',
-        type: 'AR',
         damage: [26, 24, 19, 18],
         range: [13, 32, 51],
         firerate: 652,
@@ -283,7 +260,6 @@ const gunStats = [
     {
         id: 1160,
         name: 'FR .556',
-        type: 'AR',
         damage: [25, 19],
         range: [26],
         firerate: 675,
@@ -295,7 +271,6 @@ const gunStats = [
     {
         id: 1170,
         name: 'AS VAL',
-        type: 'AR', 
         damage: [28, 25, 22, 19],
         range: [7, 20, 30],
         firerate: 800,
@@ -317,7 +292,6 @@ const gunStats = [
     {
         id: 1180,
         name: 'CR-56 AMAX',
-        type: 'AR', 
         damage: [25, 24, 20, 16],
         range: [13, 18, 26],
         firerate: 710,
@@ -333,8 +307,7 @@ const gunStats = [
     {
         id: 1181,
         name: 'CR-56 AMAX (M67)',
-        type: 'AR', 
-        damage: [48, 24, 20, 16],
+        damage: [48, 44, 35, 31],
         range: [26, 36, 52],
         firerate: 280,
         hitbox: {
@@ -347,7 +320,6 @@ const gunStats = [
     {
         id: 1190,
         name: 'M13',
-        type: 'AR', 
         damage: [24, 18, 16],
         range: [25, 39],
         firerate: 880,
@@ -368,7 +340,6 @@ const gunStats = [
     {
         id: 3010,
         name: 'S36',
-        type: 'LMG',
         damage: [30, 23, 18],
         range: [16, 38],
         firerate: 750,
@@ -379,7 +350,6 @@ const gunStats = [
     {
         id: 3020,
         name: 'UL736',
-        type: 'LMG',
         damage: [25, 22, 20, 18],
         range: [26, 36, 56],
         firerate: 620,
@@ -390,7 +360,6 @@ const gunStats = [
     {
         id: 3030,
         name: 'RPD',
-        type: 'LMG',
         damage: [31, 23, 18],
         range: [21, 56],
         firerate: 666,
@@ -401,7 +370,6 @@ const gunStats = [
     {
         id: 3040,
         name: 'M4LMG',
-        type: 'LMG',
         damage: [32, 23, 19],
         range: [21, 51],
         firerate: 633,
@@ -412,7 +380,6 @@ const gunStats = [
     {
         id: 3050,
         name: 'Chopper',
-        type: 'LMG',
         damage: [25, 21, 17],
         range: [10, 36],
         firerate: 750,
@@ -423,7 +390,6 @@ const gunStats = [
     {
         id: 3060,
         name: 'Holger 26',
-        type: 'LMG',
         damage: [31, 25, 21, 19],
         range: [16, 20, 50],
         firerate: 710,
@@ -438,7 +404,6 @@ const gunStats = [
     {
         id: 3070,
         name: 'Hades',
-        type: 'LMG',
         damage: [29, 22, 19],
         range: [24, 36],
         firerate: 650,
@@ -453,7 +418,6 @@ const gunStats = [
     {
         id: 3071,
         name: 'Hades (RFB)',
-        type: 'LMG',
         damage: [31, 22, 19],
         range: [17, 25],
         firerate: 730,
@@ -468,7 +432,6 @@ const gunStats = [
     {
         id: 4010,
         name: 'RUS-79U',
-        type: 'SMG',
         damage: [28, 24, 18, 15],
         range: [5, 19, 33],
         firerate: 769,
@@ -485,7 +448,6 @@ const gunStats = [
     {
         id: 4020,
         name: 'Chicom',
-        type: 'SMG',
         damage: [27, 24, 20, 17],
         range: [9, 16, 21],
         firerate: 788,
@@ -503,7 +465,6 @@ const gunStats = [
     {
         id: 4030,
         name: 'PDW-57',
-        type: 'SMG',
         damage: [27, 22, 17, 15],
         range: [6, 15, 28],
         firerate: 745,
@@ -520,7 +481,6 @@ const gunStats = [
     {
         id: 4040,
         name: 'Razorback',
-        type: 'SMG',
         damage: [30, 23, 19],
         range: [16, 29],
         firerate: 630,
@@ -532,7 +492,6 @@ const gunStats = [
     {
         id: 4041,
         name: 'Razorback (RF)',
-        type: 'SMG',
         damage: [30, 23, 19],
         range: [16, 29],
         firerate: 703,
@@ -544,7 +503,6 @@ const gunStats = [
     {
         id: 4050,
         name: 'MSMC',
-        type: 'SMG',
         damage: [25, 21, 18, 16],
         range: [8, 14, 19],
         firerate: 916,
@@ -556,7 +514,6 @@ const gunStats = [
     {
         id: 4060,
         name: 'HG 40',
-        type: 'SMG',
         damage: [26, 24, 20, 17],
         range: [10, 16, 25],
         firerate: 670,
@@ -574,7 +531,6 @@ const gunStats = [
     {
         id: 4070,
         name: 'Pharo',
-        type: 'SMG',
         damage: [34, 26, 21, 19, 16],
         range: [12, 19, 24, 30],
         firerate: 790,
@@ -590,7 +546,6 @@ const gunStats = [
     {
         id: 4080,
         name: 'GKS',
-        type: 'SMG',
         damage: [28, 24, 19],
         range: [20, 30],
         firerate: 600,
@@ -602,7 +557,6 @@ const gunStats = [
     {
         id: 4090,
         name: 'Cordite',
-        type: 'SMG',
         damage: [24, 22, 19, 18, 16],
         range: [8, 19, 26, 33],
         firerate: 800,
@@ -614,7 +568,6 @@ const gunStats = [
     {
         id: 4100,
         name: 'QQ9',
-        type: 'SMG',
         damage: [25, 23, 17, 15],
         range: [11, 16, 21],
         firerate: 830,
@@ -626,7 +579,6 @@ const gunStats = [
     {
         id: 4101,
         name: 'QQ9 (10mm)',
-        type: 'SMG',
         damage: [26, 23, 17, 15],
         range: [12, 18, 24],
         firerate: 810,
@@ -638,7 +590,6 @@ const gunStats = [
     {
         id: 4110,
         name: 'Fennec',
-        type: 'SMG',
         damage: [23, 18, 13, 12],
         range: [12, 17, 24],
         firerate: 1110,
@@ -658,7 +609,6 @@ const gunStats = [
     {
         id: 4120,
         name: 'AGR 556',
-        type: 'SMG',
         damage: [25, 21, 17, 15],
         range: [12, 16, 24],
         firerate: 755,
@@ -670,7 +620,6 @@ const gunStats = [
     {
         id: 4121,
         name: 'AGR 556 (5.56)',
-        type: 'SMG',
         damage: [27, 23, 20, 18],
         range: [15, 20, 30],
         firerate: 682,
@@ -682,7 +631,6 @@ const gunStats = [
     {
         id: 4130,
         name: 'QXR',
-        type: 'SMG',
         damage: [22, 19, 16, 12],
         range: [8, 15, 25],
         firerate: 889,
@@ -695,7 +643,6 @@ const gunStats = [
     {
         id: 4131,
         name: 'QXR (EB)',
-        type: 'SMG',
         damage: [22, 19, 16, 12],
         range: [8, 15, 20],
         firerate: 1029,
@@ -708,7 +655,6 @@ const gunStats = [
     {
         id: 4140,
         name: 'PP19 Bizon',
-        type: 'SMG',
         damage: [26, 24, 22, 16],
         range: [15, 21, 31],
         firerate: 650,
@@ -721,7 +667,6 @@ const gunStats = [
     {
         id: 4141,
         name: 'PP19 Bizon (LC)',
-        type: 'SMG',
         damage: [26, 24, 22, 16],
         range: [12, 17, 25],
         firerate: 650,
@@ -732,7 +677,6 @@ const gunStats = [
     {
         id: 4150,
         name: 'MX9',
-        type: 'SMG',
         damage: [30, 24, 16, 15],
         range: [12, 16, 21],
         firerate: 850,
@@ -745,7 +689,6 @@ const gunStats = [
     {
         id: 4151,
         name: 'MX9 (LC)',
-        type: 'SMG',
         damage: [30, 24, 16, 15],
         range: [12, 16, 21],
         firerate: 850,
@@ -758,7 +701,6 @@ const gunStats = [
     {
         id: 7010,
         name: 'J358',
-        type: 'HG',
         damage: [60, 40, 34, 25],
         range: [19, 21, 35],
         firerate: 380,
@@ -769,7 +711,6 @@ const gunStats = [
     {
         id: 7020,
         name: 'MW11',
-        type: 'HG',
         damage: [26, 20, 17, 16],
         range: [21, 25, 28],
         firerate: 440,
@@ -780,7 +721,6 @@ const gunStats = [
     {
         id: 7030,
         name: '.50 GS',
-        type: 'HG',
         damage: [70, 55, 48],
         range: [11, 26],
         firerate: 330,
@@ -791,7 +731,6 @@ const gunStats = [
     {
         id: 7040,
         name: 'Renetti',
-        type: 'HG',
         damage: [30, 23, 21, 19],
         range: [25, 36, 45],
         firerate: 720,
@@ -808,7 +747,6 @@ const gunStats = [
     {
         id: 7050,
         name: 'Crossbow',
-        type: 'HG',
         damage: [80],
         range: [],
         firerate: 25.5,
